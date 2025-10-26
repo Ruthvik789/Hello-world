4f08238fed914756f815399665c289825bb0c0d9
Ruthvik789 <ruthvikruthvik599@gmail.com>
2025-10-26 17:49:31 +0530

    One

---
+++ Program1.java
@@ -0,0 +1,13 @@
+class Program1{
+public static void main(String[]args){
+    System.out.println("Hello World");
+    System.out.println("Size of Byte:"+Byte.SIZE+"bits");
+     System.out.println("Size of Short:"+Short.SIZE+"bits");
+      System.out.println("Size of Integer:"+Integer.SIZE+"bits");
+       System.out.println("Size of Long:"+Long.SIZE+"bits");
+        System.out.println("Size of Float:"+Float.SIZE+"bits");
+         System.out.println("Size of Double:"+Double.SIZE+"bits");
+           System.out.println("Size of Char:"+Character.SIZE+"bits");
+             System.out.println("Size of Boolean:Typically 1 bit (implementation - dependent0)");
+}
+}
